3d94cd50bbd0ea744f806a4237ead0e54bdd04b6
CervinoB <jcervinobarbosa@gmail.com>
2022-07-15 15:48:08 -0300

    Implementação do contador ate 10 e -10, e numeros negativos vermelhos

--- introducao_ao_javascript/assets/js/scripts.js
+++ introducao_ao_javascript/assets/js/scripts.js
@@ -2,11 +2,25 @@ var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
 function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber<10){
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    verify(currentNumber);
 }
 
 function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber>-10){
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    verify(currentNumber);
+}
+
+function verify(){
+    if(currentNumber>=0){
+        currentNumberWrapper.style.color='black'
+    }else{
+        currentNumberWrapper.style.color='red'
+    }
 }
