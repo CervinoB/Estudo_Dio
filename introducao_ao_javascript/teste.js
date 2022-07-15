e651d51d9d218ff27c4a4adfdbf6bd733d2060b6
CervinoB <jcervinobarbosa@gmail.com>
2022-07-15 14:21:55 -0300

    Ajuste no teste.js

--- introducao_ao_javascript/teste.js
+++ introducao_ao_javascript/teste.js
@@ -3,6 +3,8 @@ function returnEvenValues(array){
     for(let i = 0; i < array.length; i++){
         if(array[i]%2 == 0){
             evenNums.push(array[i]);
+        }else{
+            console.log(`${array[i]} nao eh par`);
         }
     }
     console.log('os numeros pares sao:', evenNums);
